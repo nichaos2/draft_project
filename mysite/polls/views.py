f4d1414afcbe3b054e7bb9a8b0b64311fffd22b3
nioannou <nick.ioannou86@gmail.com>
2022-03-09 12:58:54 +0200

    add detail view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
+from django.http import Http404
 # from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Question
 
 
@@ -14,7 +15,8 @@ def index(request):
     # return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
