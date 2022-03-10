5a1e7707204eb667cc5afa9ecaf237905287a6b1
nioannou <nick.ioannou86@gmail.com>
2022-03-10 14:55:40 +0200

    create serializers

---
+++ mysite/polls/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework_json_api import serializers
+from .models import Question, Choice
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('question_text', 'pub_date')
+
+class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ('choice_text', 'votes', 'question')
