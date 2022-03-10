3e0c2afd75e2ca604129a7dd634ee18ca3504e38
nioannou <nick.ioannou86@gmail.com>
2022-03-10 15:36:58 +0200

    modify to ModelSerializer

--- mysite/polls/serializers.py
+++ mysite/polls/serializers.py
@@ -2,13 +2,13 @@ from rest_framework_json_api import serializers
 from .models import Question, Choice
 
 
-class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ("question_text", "pub_date")
 
 
-class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
+class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ("choice_text", "votes", "question")
